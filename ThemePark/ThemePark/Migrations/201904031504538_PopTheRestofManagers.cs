ff5ba50aa227424959ad1a734fe446b829f91704
Kristopher Manceaux <krismanceaux@hotmail.com>
2019-04-03 10:54:47 -0500

    Seeded data

---
+++ ThemePark/ThemePark/Migrations/201904031504538_PopTheRestofManagers.cs
@@ -0,0 +1,16 @@
+namespace ThemePark.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopTheRestofManagers : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
