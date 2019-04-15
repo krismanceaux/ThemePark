72d5e5ebcc41bd464b775eeb3e00f1680ec6987f
jchan4997 <q812444566@gmail.com>
2019-04-15 12:51:57 -0500

    Update UI for pleaseant looks

---
+++ ThemePark/ThemePark/Controllers/ThingsToDoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThemePark.Controllers
+{
+    public class ThingsToDoController : Controller
+    {
+        public ActionResult UH_Ride()
+        {
+            return View();
+        }
+
+        public ActionResult CS_Train()
+        {
+            return View();
+        }
+
+        public ActionResult TEXAS_Coaster()
+        {
+            return View();
+        }
+
+        public ActionResult DBMS_Drive()
+        {
+            return View();
+        }
+
+
+        public ActionResult UH_RideS()
+        {
+            return View();
+        }
+
+        public ActionResult CS_TrainS()
+        {
+            return View();
+        }
+
+        public ActionResult TEXAS_CoasterS()
+        {
+            return View();
+        }
+
+        public ActionResult DBMS_DriveS()
+        {
+            return View();
+        }
+    }
+}
