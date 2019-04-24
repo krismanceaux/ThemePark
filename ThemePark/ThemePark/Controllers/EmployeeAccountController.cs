ae5355c1a11cb66695aea4cf612bd6105ed69db7
Kristopher Manceaux <krismanceaux@hotmail.com>
2019-04-23 21:21:16 -0500

    Last

--- ThemePark/ThemePark/Controllers/EmployeeAccountController.cs
+++ ThemePark/ThemePark/Controllers/EmployeeAccountController.cs
@@ -22,28 +22,38 @@ namespace ThemePark.Controllers
         {
             if (ModelState.IsValid)
             {
+
                 //need to get the Employee ID from the login table
                 //Use that ID to identify if that employee is a manager
                 var context = new TPContext();
-                var EmployeeLogin = context.EmployeeLogins.Single(x => x.LoginEmail == userAuthInfo.Username && x.Pswd == userAuthInfo.Password);
-                if (EmployeeLogin != null && context.MANAGED_BY.Any(x=> x.EmployeeID == EmployeeLogin.EmployeeID))
+                if (context.EmployeeLogins.Any(x => x.LoginEmail == userAuthInfo.Username && x.Pswd == userAuthInfo.Password) == true)
                 {
-                    ApplicationSession.AccessLevel = "Manager";
-                    ApplicationSession.Username = userAuthInfo.Username;
-                    return Redirect("/Home/ManagerHub");
-                }
-                else if(EmployeeLogin != null)
-                {
-                    ApplicationSession.AccessLevel = "Employee";
-                    ApplicationSession.Username = userAuthInfo.Username;
-                    return Redirect("/Home/EmployeeHub");
+                    var EmployeeLogin = context.EmployeeLogins.Single(x => x.LoginEmail == userAuthInfo.Username && x.Pswd == userAuthInfo.Password);
+                    if (EmployeeLogin != null && context.MANAGED_BY.Any(x => x.EmployeeID == EmployeeLogin.EmployeeID))
+                    {
+                        ApplicationSession.AccessLevel = "Manager";
+                        ApplicationSession.Username = userAuthInfo.Username;
+                        return Redirect("/Home/ManagerHub");
+                    }
+                    else if (EmployeeLogin != null)
+                    {
+                        ApplicationSession.AccessLevel = "Employee";
+                        ApplicationSession.Username = userAuthInfo.Username;
+                        return Redirect("/Home/EmployeeHub");
+                    }
+                    else
+                    {
+                        // Display error
+                        ModelState.AddModelError(string.Empty, "Invalid Username or Password");
+                        return View(userAuthInfo);
+
+                    }
                 }
                 else
                 {
                     // Display error
                     ModelState.AddModelError(string.Empty, "Invalid Username or Password");
                     return View(userAuthInfo);
-
                 }
             }
             else
